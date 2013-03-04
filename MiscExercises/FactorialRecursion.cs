ae9d1c556b04adc8d821a849d37a6a97bf5ea390
dl <444c43@gmail.com>
2013-03-04 17:43:06 -0500

    Merge misc repo exercises into single project

---
+++ MiscExercises/FactorialRecursion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscExercises
+{
+    public class FactorialRecursion
+    {
+        public static long FindFactorial(long val)
+        {
+            while (val > 1)
+            {
+                return val * FindFactorial(val - 1);
+            }
+            return 1;
+        }
+    }
+}
---
+++ MiscExercises/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscExercises
+{
+    class MiscExercisesMain
+    {
+        static void Main(string[] args)
+        {
+            DisplayResults();
+        }
+        static void DisplayResults()
+        {
+            Console.Write("Factorial of 7 = ");
+            Console.WriteLine(FactorialRecursion.FindFactorial(7));
+            Console.WriteLine("");
+
+            ListManipulation.ListManipulationBegin();
+            Console.WriteLine("");
+        }
+    }
+}
