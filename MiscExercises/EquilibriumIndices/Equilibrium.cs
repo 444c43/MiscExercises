5ad63bbc45fdb068d82f961088e25d3ce88f3fa5
dl <444c43@gmail.com>
2013-03-19 15:57:35 -0400

    write passing equilibrium code

--- MiscExercises/EquilibriumIndices/Equilibrium.cs
+++ MiscExercises/EquilibriumIndices/Equilibrium.cs
@@ -8,8 +8,22 @@ namespace MiscExercises.EquilibriumIndices
 {
     public class Equilibrium
     {
-        public static int FirstEquilibrium(int[] A, int N)
+        public static int FindFirst(int[] A, int N)
         {
+            long RightSide = A.Sum();
+            long LeftSide = 0;
+
+            for (int x = 0; x <= A.Length - 1; x++)
+            {
+                RightSide -= A[x];
+
+                if (RightSide == LeftSide)
+                {
+                    return x;
+                }
+
+                LeftSide += A[x];
+            }
             return -1;
         }
     }
