5a9cccf39d427ef224ad90eb7dd3120702e505d2
dl <444c43@gmail.com>
2013-04-30 10:31:44 -0400

    Collatz.cs: modify previous tests, add reusability test

--- MiscExercisesUnitTests/CollatzConjectureTests.cs
+++ MiscExercisesUnitTests/CollatzConjectureTests.cs
@@ -10,12 +10,14 @@ namespace MiscExercisesUnitTests
     public class CollatzConjectureTests
     {
         CollatzConjecture Conjecture;
+        List<long> FirstCompareList = new List<long> 
+            { 6, 3, 10, 5, 16, 8, 4, 2, 1 };
+        List<long> SecondCompareList = new List<long> { 4, 2, 1 };
 
         [TestMethod]
         public void DefaultConstructorCreatesThreeElementList()
         {
             Conjecture = new CollatzConjecture();
-
             Assert.AreEqual(3, Conjecture.Sequence.Count);
         }
 
@@ -23,18 +25,22 @@ namespace MiscExercisesUnitTests
         public void DefaultConstructorValues()
         {
             Conjecture = new CollatzConjecture();
-            List<long> CompareList = new List<long> { 4, 2, 1 };
-
-            Assert.IsTrue(CompareList.SequenceEqual(Conjecture.Sequence));
+            Assert.IsTrue(SecondCompareList.SequenceEqual(Conjecture.Sequence));
         }
 
         [TestMethod]
         public void CalculatedResults()
         {
             Conjecture = new CollatzConjecture(6);
-            List<long> CompareList = new List<long> { 6, 3, 10, 5, 16, 8, 4, 2, 1 };
+            Assert.IsTrue(FirstCompareList.SequenceEqual(Conjecture.Sequence));
+        }
 
-            Assert.AreEqual(true, CompareList.SequenceEqual(Conjecture.Sequence));
+        [TestMethod]
+        public void TestReusability()
+        {
+            Conjecture = new CollatzConjecture(6);
+            Conjecture.EnterNewSeedNumber(4);
+            Assert.IsTrue(SecondCompareList.SequenceEqual(Conjecture.Sequence));
         }
     }
 }
