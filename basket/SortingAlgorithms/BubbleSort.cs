4176eb698772c47b0e864e26cb2fcb66680ba843
dl <444c43@gmail.com>
2013-06-04 15:38:46 -0400

    revise directory names & adjust some files from previous merge

---
+++ basket/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        #region fields
+        private List<int> SortedList;
+        private bool IsSorted;
+        public int PassCount { get; private set; }
+        #endregion
+
+        #region constructor
+        public BubbleSort()
+        {
+            SortedList = new List<int>();
+            IsSorted = false;
+            PassCount = 0;
+        }
+        #endregion
+
+        public List<int> SortList(List<int> list)
+        {
+            SortedList = list;
+
+            while (IsSorted == false)
+            {
+                IsSorted = true;
+                IterateList();
+                PassCount++;
+            }
+            return SortedList;
+        }
+
+        #region private methods
+        private void IterateList()
+        {
+            for (int i = 1; i < SortedList.Count; i++)
+            {
+                CompareListMembers(i);
+            }
+        }
+
+        private void CompareListMembers(int i)
+        {
+            if (SortedList[i] < SortedList[i - 1])
+            {
+                SortedList.Reverse(i - 1, 2);
+                IsSorted = false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ basket/SortingAlgorithms/README.md
@@ -0,0 +1,7 @@
+SortingAlgorithms
+=================
+
+an exercise in writing various sorting algorithms.
+
+this exercise is to improve my C# coding skills. i'll let tests drive my coding
+as much as i can, coding a variety of sorting algorithms.
