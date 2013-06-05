2314580c1292a14daaab9e119363f6844a2bd00f
dl <444c43@gmail.com>
2013-06-04 22:40:21 -0400

    add failing BubbleSort Test

---
+++ tests/BubbleSortTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FruitBasket;
+using System.Linq;
+using SortingAlgorithms;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MiscExercisesUnitTests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        List<int> ListToSort = new List<int> { 6, 5, 4, 3, 2, 1 };
+        List<int> CompareList = new List<int> { 1, 2, 3, 4, 5, 6 };
+        BubbleSort Sort;
+
+        [TestMethod]
+        public void TestSortResult()
+        {
+            Sort = new BubbleSort(ListToSort);
+
+            Assert.IsTrue(CompareList.SequenceEqual(Sort.SortedList));
+        }
+    }
+}
